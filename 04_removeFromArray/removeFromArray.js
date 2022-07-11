a98738b4f90fbfb734108784e7df8bc950a7c9b2
nicholas-shaw <hello@nicholas-shaw.com>
2022-07-11 22:59:03 +1000

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...values) {
+  for (i = 0; i < values.length; i++) {
+    const indexToRemove = array.indexOf(values[i]);
+    if (indexToRemove >= 0) {
+      array.splice(indexToRemove, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
