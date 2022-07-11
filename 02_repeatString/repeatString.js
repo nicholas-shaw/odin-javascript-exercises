8dd2b57c47816731b064a974ab72504bac2521b4
nicholas-shaw <hello@nicholas-shaw.com>
2022-07-11 10:07:36 +1000

    Completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeatValue) {
+  if (repeatValue >= 0) {
+    let stringToReturn = "";
+    for (let i = 0; i < repeatValue; i++) {
+      stringToReturn += string;
+    }
+    return stringToReturn;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
