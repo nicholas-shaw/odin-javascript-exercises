50a03662b2fb2daf1461bb6f30837ba7a255d223
nicholas-shaw <hello@nicholas-shaw.com>
2022-07-13 15:43:52 +1000

    Solution to exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  // * and / 10 Calculation to automatically round to 0 or 1 digit.
+  return Math.round((temp - 32) * (5 / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  // * and / 10 Calculation to automatically round to 0 or 1 digit.
+  return Math.round((temp * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
