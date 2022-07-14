3501561d33a890db8b2a106964f3ff85ca9fea0f
nicholas-shaw <hello@nicholas-shaw.com>
2022-07-14 16:24:37 +1000

    Added solution to excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (member) {
+  let result = [];
+  if (member > 0) {
+    for (i = 0; i < member; i++) {
+      if (result.length < 2) {
+        result.push(1);
+      } else {
+        result.push(result[result.length - 1] + result[result.length - 2]);
+      }
+    }
+    return result[result.length - 1];
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
