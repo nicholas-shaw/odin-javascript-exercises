67d4da83d423300cb4c81aa394cb2afb9b4a3e30
nicholas-shaw <hello@nicholas-shaw.com>
2022-07-14 14:39:02 +1000

    Solution to exercise 8

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (testPhrase) {
+  const sanitisedPhrase = testPhrase.replace(/\W+/g, "").toUpperCase();
+  let reversedPhrase = Array.from(sanitisedPhrase).reverse().join("");
+  if (sanitisedPhrase === reversedPhrase) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
